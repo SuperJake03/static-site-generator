38ddfd58df427c9293be0434895ca13d6a846e90
Superjake03 <jacoblopez0308@gmail.com>
2026-02-01 01:04:49 -0600

    helper funcs for extracting images and links from md

--- src/test_inline_markdowns.py
+++ src/test_inline_markdowns.py
@@ -1,6 +1,10 @@
 import unittest
 
-from inline_markdowns import split_nodes_delimiter
+from inline_markdowns import (
+    extract_markdown_images,
+    extract_markdown_links,
+    split_nodes_delimiter,
+)
 from textnode import TextNode, TextType
 
 
@@ -125,6 +129,24 @@ class TestInlineMarkdown(unittest.TestCase):
                 TextNode(" word", TextType.PLAIN),
             ],
             new_nodes,
+        )
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        )
+        self.assertListEqual(
+            [
+                ("to boot dev", "https://www.boot.dev"),
+                ("to youtube", "https://www.youtube.com/@bootdotdev"),
+            ],
+            matches,
         )
 
 
