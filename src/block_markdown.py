00b01d74442ff42c5c666cab173415b7c5188904
Superjake03 <jacoblopez0308@gmail.com>
2026-02-03 20:36:01 -0600

    block type completed

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,15 @@
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
 def markdown_to_blocks(markdown):
     parts = markdown.split("\n\n")
     blocks = []
@@ -6,3 +18,29 @@ def markdown_to_blocks(markdown):
         if len(block) != 0:
             blocks.append(block)
     return blocks
+
+
+def block_to_block_type(block):
+    lines = block.splitlines()
+    for num_hashes in range(1, 7):
+        prefix = "#" * num_hashes + " "
+        if lines[0].startswith(prefix):
+            return BlockType.HEADING
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return BlockType.CODE
+    for i in range(len(lines)):
+        if not lines[i].startswith(">"):
+            break
+        if i == len(lines) - 1:
+            return BlockType.QUOTE
+    for i in range(len(lines)):
+        if not lines[i].startswith("- "):
+            break
+        if i == len(lines) - 1:
+            return BlockType.UNORDERED_LIST
+    for i in range(len(lines)):
+        if not lines[i].startswith(f"{i + 1}. "):
+            break
+        if i == len(lines) - 1:
+            return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
