232e798f2bcb280e9f19833e6e86df0233f8314f
Superjake03 <jacoblopez0308@gmail.com>
2026-02-08 16:20:04 -0600

    final steps

--- src/generate_content.py
+++ src/generate_content.py
@@ -27,3 +27,7 @@ def generate_page(from_path, template_path, dest_path):
         os.makedirs(dir_name, exist_ok=True)
     with open(dest_path, "w") as f:
         f.write(new_html)
+
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    raise NotImplementedError
